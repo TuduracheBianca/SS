88e161c0b4db580592f279d641ad9358404a35e5
Tudurache Bianca-Felicia <bianca.tudurache@stud.ubbcluj.ro>
2025-11-23 18:08:50 +0200

    Revert "KK"
    This reverts commit bb8664aaf8907e8e925c60a06ccee9a046cc0367.

--- application/views/home/home_profesori.php
+++
@@ -1,8 +0,0 @@
-
-<div class="card">
-    <div class="card-header"><h4>Profesor Home</h4></div>
-    <div class="card-body">
-        <!-- blank starter area for profesor -->
-        <p>Welcome, profesor.</p>
-    </div>
-</div>
--- application/views/home/home_studenti.php
+++
@@ -1,8 +0,0 @@
-
-<div class="card">
-    <div class="card-header"><h4>Student Home</h4></div>
-    <div class="card-body">
-        <!-- blank starter area for student -->
-        <p>Welcome, student.</p>
-    </div>
-</div>
